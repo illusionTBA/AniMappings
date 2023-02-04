b62704cf047cb273822975e1136b805a5f6946e8
illusionTBA <illusionztba@gmail.com>
2023-02-04 16:09:33 -0500

    null => undefined

--- src/mappings/kitsu.ts
+++ src/mappings/kitsu.ts
@@ -18,7 +18,7 @@ const kitsu = async (title: string) => {
   } catch (error) {
     // console.error(error);
     console.log(`[-] Failed to get mappings for ${title} on Kitsu`);
-    return null;
+    return undefined;
   }
 };
 
